bc40cbadddd80d7fcfe566f526884b427b95a569
Kvothik <cromeans15@gmail.com>
2019-09-27 14:48:37 -0600

    created skeleton

---
+++ src/com/skilldistillery/jets/app/AirField.java
@@ -0,0 +1,11 @@
+package com.skilldistillery.jets.app;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AirField {
+	List<ArrayList<Jet>> jets = new ArrayList<>();
+
+	public AirField() {
+	}
+}
---
+++ src/com/skilldistillery/jets/app/BasicJet.java
@@ -0,0 +1,21 @@
+package com.skilldistillery.jets.app;
+
+public class BasicJet extends Jet{
+
+	public BasicJet(String model, double speed, int range, long price) {
+		super(model, speed, range, price);
+	}
+
+	@Override
+	public void fly() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public double getSpeedInMach() {
+		double mach = (getSpeed() / 767.269);
+		return mach;
+	}
+
+}
